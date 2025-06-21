df0afd0b2ab4202c373ff7089798e21686f964c6
Gabriela Pinheiro <gabriela.dasilvapinheiro@thriveworks.com>
2025-06-21 01:19:23 -0300

    Time: 1 ms (98.82%), Space: 69.1 MB (78.28%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.ts
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.ts
@@ -1,6 +1,6 @@
 function findMaxAverage(nums: number[], k: number): number {
-    let currentSum = 0;
-    let maxSum = 0;
+    let currentSum = 0
+    let maxSum = 0
 
     for(let i = 0; i < k; i++) {
         currentSum += nums[i]
@@ -9,8 +9,8 @@ function findMaxAverage(nums: number[], k: number): number {
     maxSum = currentSum
 
     for(let i = k; i < nums.length; i++) {
-        currentSum += nums[i] - nums[i - k]
-        maxSum = Math.max(maxSum, currentSum)
+        currentSum += nums[i] - nums[i - k] //currentSum = currentSum + new - old
+        maxSum = Math.max(currentSum, maxSum)
     }
 
     return maxSum / k
