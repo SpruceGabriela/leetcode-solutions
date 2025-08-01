0a29b7a78094506c43662434de6230fae4554011
Gabriela Pinheiro <gabriela.dasilvapinheiro@thriveworks.com>
2025-07-31 21:25:01 -0300

    Time: 17 ms (96.44%), Space: 72 MB (63.73%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.ts
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.ts
@@ -1,22 +1,17 @@
 function containsNearbyDuplicate(nums: number[], k: number): boolean {
-   let left = 0
-   let right = 0
    let set = new Set<number>();
+   let i = 0;
 
-   while(right < nums.length) {
-        const char = nums[right]
-
-        while(set.has(char)) {
-            if(right - left <= k) {
-                return true
-            }
-
-            set.delete(nums[left])
-            left++
+    for(let j = 0; j < nums.length; j++) {
+        while(set.size > k) {
+            set.delete(nums[i])
+            i++
         }
-        set.add(nums[right])
-        right++
-   }
+        if(set.has(nums[j])) {
+            if(Math.abs(i - j) <= k) return true
+        }
+        set.add(nums[j])
+    }
 
-   return false
+   return false;
 };
